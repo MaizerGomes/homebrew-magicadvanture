dcaf4ff8894c520c74b2cee44c32433354088d1f
Maizer Gomes <maizer.gomes@gmail.com>
2026-04-24 21:50:13 +0200

    Update to v7.3

--- magicadventure.rb
+++ magicadventure.rb
@@ -1,13 +1,13 @@
 class Magicadventure < Formula
   desc "A terminal RPG with slot-based saves, turn-based combat, and optional multiplayer"
   homepage "https://github.com/MaizerGomes/magicadvanture"
-  version "7.2"
-  url "https://github.com/MaizerGomes/magicadvanture/releases/download/v7.2/magicadventure-mac-arm"
+  version "7.3"
+  url "https://github.com/MaizerGomes/magicadvanture/releases/download/v7.3/magicadventure-mac-arm"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/MaizerGomes/magicadvanture/releases/download/v7.2/magicadventure-mac-64"
+      url "https://github.com/MaizerGomes/magicadvanture/releases/download/v7.3/magicadventure-mac-64"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
